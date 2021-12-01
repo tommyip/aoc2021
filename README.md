f2411e48a08c932d4746ae82391880f9a7fcb25c
Thomas Ip <thomas@ipthomas.com>
2021-12-01 14:48:46 +0800

    🎄 Day 1

---
+++ README.md
@@ -0,0 +1,8 @@
+# 🎄 Advent Of Code 2021 🎄
+
+https://adventofcode.com/2021
+
+Running the solutions:
+```bash
+python day<XX>.py < inputs/day<XX>.txt
+```
---
+++ day01.py
@@ -0,0 +1,20 @@
+import sys
+
+depths = [int(x) for x in sys.stdin.readlines()]
+
+count_1 = 0
+for i in range(1, len(depths)):
+    if depths[i] > depths[i-1]:
+        count_1 += 1
+
+print('Part 1:', count_1)
+
+count_2 = 0
+for i in range(3, len(depths)):
+    middle_sum = depths[i-1] + depths[i-2]
+    cur = middle_sum + depths[i]
+    prev = middle_sum + depths[i-3]
+    if cur > prev:
+        count_2 += 1
+
+print('Part 2:', count_2)
